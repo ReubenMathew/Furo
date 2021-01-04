a3d76616834d785fbbe81eb01a0a1b6170ca6c0f
Reuben Ninan <intelphysic@gmail.com>
2021-01-04 03:51:39 -0500

    darkened dropdown menu text

--- pages/index.js
+++ pages/index.js
@@ -62,11 +62,11 @@ export default function Home() {
           <div style={{display: showMenu? "block" : "none"}} class="">
             <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
 
-              <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">About</a>
+              <a href="#" class="text-gray-600 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">About</a>
 
-              <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Changelog</a>
+              <a href="#" class="text-gray-600 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Changelog</a>
 
-              <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Features</a>
+              <a href="#" class="text-gray-600 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Features</a>
 
             </div>
           </div>
