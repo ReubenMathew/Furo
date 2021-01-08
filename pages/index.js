0f8a448495445627f3265917751f5f7f334264d7
Reuben Ninan <intelphysic@gmail.com>
2021-01-08 01:54:00 -0500

    Footer changes and about page updates

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export default function Home() {
   
@@ -19,7 +20,7 @@ export default function Home() {
           <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
             <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/350x100"/>
             <div class="text-center lg:w-2/3 w-full">
-              <h1 class="title-font md:text-6xl sm:text-5xl text-4xl mb-4 font-black  text-black">Make your workflow more intentful</h1>
+              <h1 class="title-font md:text-6xl sm:text-5xl text-4xl mb-4 font-black text-black">Make your workflow more intentful</h1>
               <div className="flex justify-center">
                 <p class="mb-8 leading-relaxed text-center w-full sm:w-3/5">Optimize your work by focusing your attention on one task at a time, built for digital minimalists and productivity power-users.</p>
               </div>
@@ -33,8 +34,8 @@ export default function Home() {
 
       </main>
 
-      <footer className="">
-      </footer>
+      <Footer/>
+
     </div>
   )
 }
