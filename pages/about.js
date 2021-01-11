acf124a07180f4a285b7b4ac805a5cbadc720459
Reuben Ninan <intelphysic@gmail.com>
2021-01-10 22:53:44 -0500

    about section complete

--- pages/about.js
+++ pages/about.js
@@ -38,12 +38,10 @@ export default function Home() {
                     </div>
                     </div>
                     <div class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-                    <p class="leading-relaxed text-lg mb-4">Fill this in when I get something clever to write. Make sure you credit Cole with the idea.</p>
-                    <a class="text-blue-500 inline-flex items-center">Learn More
-                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                        </svg>
-                    </a>
+                    <p class="leading-relaxed text-lg mb-4">This concept for a digital intent app was originally designed and implemented on MacOS by <a class="text-blue-500" href="https://twitter.com/colebemis/status/1334648731287736322">Cole Bemis</a>. </p> 
+                    <p class="leading-relaxed text-lg mb-4">When I first saw the tweet from Cole demonstrating this tool I immediately recognized the potential workflow benefits that I could attain. However, being a Windows user, there was a zero chance of using it without building it myself. </p>
+                    <p class="leading-relaxed text-lg mb-4">So I did! This is something that I now use on a daily basis and can't focus without. I am distributing and building this tool in public for other Windows users who could benefit from it. </p>
+                    <a href="https://lonepinekoalasanctuary.com/product/koala-pin/" class="text-blue-500 inline-flex items-center">🐨 Save the koalas!</a>
                     </div>
                 </div>
                 </div>
